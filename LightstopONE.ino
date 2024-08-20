ca11cd16af4804c842f31406a5be11c8a1be6c7a
im-plosion <nathaliare14@gmail.com>
2024-08-19 19:46:11 -0500

    First commit

---
+++ LightstopONE.ino
@@ -0,0 +1,37 @@
+
+void setup() {
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+
+}
+
+void loop() {
+digitalWrite(8,HIGH);
+delay(50);
+digitalWrite(8,LOW);
+delay(100);
+digitalWrite(7,HIGH);
+delay(1000);
+digitalWrite(7,LOW);
+delay(100);
+digitalWrite(6,HIGH);
+delay(1500);
+digitalWrite(6,LOW);
+delay(100);
+digitalWrite(7,HIGH);
+delay(1000);
+digitalWrite(7,LOW);
+delay(150);
+
+
+
+
+
+
+
+
+
+
+}
